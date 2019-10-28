200c949b077b7ca4108ed5c463a97376e0508e96
vincent-tr <vincent@stoic.com>
2019-10-28 10:34:59 +0100

    wip web server

--- bin/server.js
+++ bin/server.js
@@ -3,10 +3,11 @@
 'use strict';
 
 require('../lib/init');
+require('../lib/portal-web-server');
 const { runServices } = require('mylife-tools-server');
 const metadataDefintions = require('../lib/metadata');
 const storeConfiguration = require('../lib/store-configuration');
 
-const services = ['store'];
+const services = ['portal-web-server'];
 const parameters = { apiServices, metadataDefintions, storeConfiguration };
 runServices({ services, ... parameters });
